d8a45a678b559557d4efaa9855e2a2d6f36b70b7
Vuk Radonjic <radonjic.vuk02@gmail.com>
2022-06-19 22:11:25 +0200

    dodao static filove

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const options = {
+    extensions: ['htm', 'html']
+}
 
+app.use(express.static('public', options))
 app.use('/api/users', require('./routes/users'))
 
 const PORT = process.env.PORT || 8080
