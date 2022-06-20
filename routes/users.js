a1302d9be9da713b0eafc03b71af9144171da4c9
Vuk Radonjic <radonjic.vuk02@gmail.com>
2022-06-20 23:22:30 +0200

    Povezao sa bazom podataka

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/Users')
 
-router.get('/', (req, res) => {
-    res.json({"test": 3})
+router.get('/', async (req, res) => {
+    if(req.query.id === undefined){
+        res.json(await User.find({}))
+    }else{
+        User.find({_id: req.query.id}, async (err, content) =>{
+            if(content === undefined){
+                return res.sendStatus(400)
+            }
+            res.json(content)
+        })
+    } 
 })
 
-module.exports = router;
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    const user = new User({
+        name: req.body.name,
+        password: req.body.password
+    })
+
+    if(!user.name || !user.password){
+        res.sendStatus(400)
+    }else{
+        console.log()
+        await user.save()
+        res.json(user)
+    }
+})
+
+// ! DODATI DELETION ZA USERE
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const options = {
     extensions: ['htm', 'html']
 }
 
+mongoose.connect('mongodb+srv://Vuk:cafe4529@ilkehubcluster.jlsfbhd.mongodb.net/?retryWrites=true&w=majority')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use(express.static('public', options))
+
 app.use('/api/users', require('./routes/users'))
 
 const PORT = process.env.PORT || 8080
